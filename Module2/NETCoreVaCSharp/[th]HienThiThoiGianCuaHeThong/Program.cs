6c79847d028828ee9a4068bbe3fffd2238cf2d48
HuynhThaoNguyen <htnguyen-git>
2020-05-04 09:08:22 +0700

    Hien thi thoi gian cua he thong

---
+++ Module2/NETCoreVaCSharp/[th]HienThiThoiGianCuaHeThong/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _th_HienThiThoiGianCuaHeThong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localTime = DateTime.Now;
+            Console.WriteLine("Thời gian hiện tại là : " + localTime);
+        }
+    }
+}
