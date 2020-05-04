b666b820317cd131dec6a298daa57ca3da1fa94f
htnguyen-git <htnguyen-git>
2020-05-04 15:43:39 +0700

    m

---
+++ Module2/MangVaPhuongThuc/[th]TimGiaTriNhoNhatSuDungPhuongThuc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _th_TimGiaTriNhoNhatSuDungPhuongThuc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11 };
+            Console.WriteLine($"Phan tu nho nhat trong mang nam o vi tri: {MinValue(arr)}");
+        }
+        public static int MinValue(int[] array)
+        {
+
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
