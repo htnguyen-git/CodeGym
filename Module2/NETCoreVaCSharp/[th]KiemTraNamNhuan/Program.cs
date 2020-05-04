4dd7b21f6b96315c71c3b54171946755a88c6d38
HuynhThaoNguyen <htnguyen-git>
2020-05-04 09:46:17 +0700

    Them bai thuc hanh : Kiem tra nam nhuan

---
+++ Module2/NETCoreVaCSharp/[th]KiemTraNamNhuan/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _th_KiemTraNamNhuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Kiem tra nam nhuan");
+            year = int.Parse(Console.ReadLine());
+            bool isDiviveBy4 = (year % 4 == 0);
+            bool leafYear = false;
+            if (isDiviveBy4)
+            {
+                bool isDivineBy100 = (year % 100 ==0);
+                if (isDivineBy100)
+                {
+                    leafYear = true;
+                }
+            }
+            if (leafYear)
+            {
+                Console.WriteLine("la Nam nhuan");
+            }
+            else
+            {
+                Console.WriteLine("Khong phai nam nhuan");
+            }
+
+        }
+    }
+}
