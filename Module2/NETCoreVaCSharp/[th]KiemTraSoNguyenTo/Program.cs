539c15588b2ef79ba3261132d07dead5b6247769
HuynhThaoNguyen <htnguyen-git>
2020-05-04 08:57:40 +0700

    Netcore va C#

---
+++ Module2/NETCoreVaCSharp/[th]KiemTraSoNguyenTo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _th_KiemTraSoNguyenTo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nhập giá trị từ bàn phím và gán nó cho biến number
+            int number;
+            Console.WriteLine("Enter a number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            //Kiểm tra số nguyên tố
+            if (number < 2)
+            {
+                Console.WriteLine(number + " không phải số nguyên tố");
+            }
+            else
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    Console.WriteLine(number + " is a prime");
+                }
+                else
+                {
+                    Console.WriteLine(number + " is not a prime");
+                }
+            }
+
+
+        }
+    }
+}
