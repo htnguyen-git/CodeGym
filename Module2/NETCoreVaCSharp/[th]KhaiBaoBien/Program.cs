03ba748c2e25ef59d098b27e750704c20ad47325
HuynhThaoNguyen <htnguyen-git>
2020-05-04 10:22:02 +0700

    them bai thuc hanh

---
+++ Module2/NETCoreVaCSharp/[th]KhaiBaoBien/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _th_KhaiBaoBien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            float f = 10.5f;
+            double d = 20.1d;
+            bool b = true;
+            char c = 'a';
+            string s = "CodeGym";
+            Console.WriteLine($"Gía trị của biến i là : {i}");
+            Console.WriteLine($"Gía trị của biến f là : {f}");
+            Console.WriteLine($"Gía trị của biến d là : {d}");
+            Console.WriteLine($"Gía trị của biến b là : {b}");
+            Console.WriteLine($"Gía trị của biến c là : {c}");
+            Console.WriteLine($"Gía trị của biến s là : {s}");
+            
+
+        }
+    }
+}
