fe344eb940f8655b8918d6e1e3aa084f5333f5cb
htnguyen-git <htnguyen-git>
2020-05-12 09:11:29 +0700

    Bai tap Acess modifier xay dung class Circle

---
+++ Module2/[bt]AcessModifier/AcessModifier/AcessModifier/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AcessModifier
+{
+    class Circle
+    {
+        private double radius = 1;
+        private string color = "red";
+
+        public Circle() { }
+        public Circle(double r)
+        {
+            radius = r;
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+        public string GetColor()
+        {
+            return color;
+        }
+        public double GetArea()
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+}
---
+++ Module2/[bt]AcessModifier/AcessModifier/AcessModifier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AcessModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle cc1 = new Circle();
+            Console.WriteLine(cc1.GetColor());
+            Console.WriteLine(cc1.GetRadius());
+            Console.WriteLine(cc1.GetArea());
+        }
+    }
+}
