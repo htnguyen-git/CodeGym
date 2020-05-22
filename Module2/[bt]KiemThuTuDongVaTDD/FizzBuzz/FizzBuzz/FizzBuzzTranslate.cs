064d22d68efb865953c1678dd88a93df44708a51
htnguyen-git <htnguyen-git>
2020-05-22 15:30:20 +0700

    bài tập thực hành fizz buzz trong 1h20p

---
+++ Module2/[bt]KiemThuTuDongVaTDD/FizzBuzz/FizzBuzz/FizzBuzzTranslate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public static class FizzBuzzTranslate
+    {
+        public static string ByteToFizzBuzz(byte number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+        }
+        public static string ConvertNumberToFizzBuzz(int number, string ketQua ="")
+        {
+            if (number == 0)
+            {
+                return ketQua;
+            }
+            return ConvertNumberToFizzBuzz(number / 10, ketQua = (ByteToFizzBuzz((byte)(number % 10))).ToString() + ketQua);
+        }
+        
+
+    }
+}
