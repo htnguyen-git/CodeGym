a42b0bedbd44ce2d7770d3b4f3b12141b3b13729
htnguyen-git <htnguyen-git>
2020-05-22 10:27:46 +0700

    Trien khai lop Linked list don gian trong 40 p

---
+++ Module2/[th]CauTrucDuLieu/TrienKhaiLopLinkedList/TrienKhaiLopLinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrienKhaiLopLinkedList
+{
+    class Node
+    {
+        public Object Data { get; set; }
+        public Node Next { get; set; }
+        
+        public Node(Object data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
