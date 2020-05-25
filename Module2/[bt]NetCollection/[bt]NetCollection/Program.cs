9e32777703f14bf8f28310aa1f27da1e6367a1b8
htnguyen-git <htnguyen-git>
2020-05-25 16:01:43 +0700

    Thao tac voi list trong 25p

---
+++ Module2/[bt]NetCollection/[bt]NetCollection/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _bt_NetCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> animals = new List<string>();
+            animals.Add("lion");
+            animals.Add("cat");
+            animals.Add("dog");
+            animals.Add("elephant");
+
+            Console.WriteLine("Danh sach hien tai");
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+            animals.Sort();
+            Console.WriteLine();
+            Console.WriteLine("Danh sach sau khi sort");
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+
+            // xoa phan tu dog
+            Console.WriteLine();
+            animals.Remove("dog");
+            Console.WriteLine("Danh sach sau khi xoa dog");
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+    }
+}
