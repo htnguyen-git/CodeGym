4fccd1ee1b5b4f7cbf57f1fdd219bb60a8131f8b
htnguyen-git <htnguyen-git>
2020-05-25 09:29:14 +0700

    Su dung dictionary de them sua xoa Book 40p

---
+++ Module2/[th]NETColectionVaDataStructure/SuDungDictionaryDeThemSuaXoaBook/SuDungDictionaryDeThemSuaXoaBook/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuDungDictionaryDeThemSuaXoaBook
+{
+    class Book
+    {
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+    }
+}
