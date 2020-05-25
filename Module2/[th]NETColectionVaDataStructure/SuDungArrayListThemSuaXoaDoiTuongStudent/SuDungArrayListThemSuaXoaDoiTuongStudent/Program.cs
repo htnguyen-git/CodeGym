cbef886edb0eaf5f980617a577649c473979311f
htnguyen-git <htnguyen-git>
2020-05-25 09:13:26 +0700

    Sử dụng Array list thêm sửa xóa đối tượng Student trong 30 p

---
+++ Module2/[th]NETColectionVaDataStructure/SuDungArrayListThemSuaXoaDoiTuongStudent/SuDungArrayListThemSuaXoaDoiTuongStudent/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace SuDungArrayListThemSuaXoaDoiTuongStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList studentList = new ArrayList();
+            studentList.Add(new Student() {Name = "Peter", Age = 20 });
+            studentList.Add(new Student() { Name = "John", Age = 21 });
+            studentList.Add(new Student() { Name = "Mark", Age = 19 });
+            foreach (Student student in studentList)
+            {
+                Console.WriteLine(student.Name + " " + student.Age);
+            }
+        }
+    }
+}
