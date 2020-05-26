91518a6911ed7939c8ad8cae74061c0ee70b7e94
htnguyen-git <htnguyen-git>
2020-05-26 17:06:26 +0700

    Chuyển đổi thập phân sang nhị phân trong 16 p

---
+++ Module2/[optional]CauTrucDuLieuStackQueueMap/ChuyenDoiThapPhanThanhNhiPhan/DecToBinConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChuyenDoiThapPhanThanhNhiPhan
+{
+    static class DecToBinConverter
+    {
+        public static string Convert(int numberDecTobin)
+        {
+            var DecToBin = new Stack<int>();
+            while (numberDecTobin > 0)
+            {
+                DecToBin.Push(numberDecTobin % 2);
+                numberDecTobin /= 2;
+            }
+            // in kết quả
+            string ketQua = "";
+            while (DecToBin.Count > 0)
+            {
+                ketQua += DecToBin.Pop().ToString();
+            }
+            return ketQua;
+        }
+    }
+}
---
+++ Module2/[optional]CauTrucDuLieuStackQueueMap/ChuyenDoiThapPhanThanhNhiPhan/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChuyenDoiThapPhanThanhNhiPhan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decNumber = 100;
+            Console.WriteLine(DecToBinConverter.Convert(decNumber)) ;
+        }
+    }
+}
