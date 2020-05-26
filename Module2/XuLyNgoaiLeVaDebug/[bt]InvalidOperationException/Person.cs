f4877f93b666c872784920379ef8ca0605070f82
htnguyen-git <htnguyen-git>
2020-05-26 15:20:30 +0700

    Bai tap Invalid Operation Exception trong 10p

---
+++ Module2/XuLyNgoaiLeVaDebug/[bt]InvalidOperationException/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _bt_InvalidOperationException
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public Person() { }
+    }
+}
---
+++ Module2/XuLyNgoaiLeVaDebug/[bt]InvalidOperationException/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _bt_InvalidOperationException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personList = new List<Person>();
+            personList.Add(new Person() {Name = "Liya", Age = 25 });
+            personList.Add(new Person() { Name = "Danya", Age = 15 });
+
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    personList.Remove(personList[i]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+        }
+    }
+}
