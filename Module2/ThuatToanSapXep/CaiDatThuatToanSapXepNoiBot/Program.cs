a4dd1d4977a70ca8d23b5e86fa019f032ba4c901
htnguyen-git <htnguyen-git>
2020-05-26 15:54:56 +0700

    Thuc hanh thuat toan sap xep trong 10p

---
+++ Module2/ThuatToanSapXep/CaiDatThuatToanSapXepNoiBot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace CaiDatThuatToanSapXepNoiBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 640, 4, 250, -12, 232, 11, -90 };
+            BubbleSort(arr);
+            string s = "";
+            foreach (var element in arr)
+            {
+                s += element + " ";
+            }
+            Console.WriteLine(s);
+        }
+        static void BubbleSort(int[] arr)
+        {
+            int i, j, temp;
+            int n = arr.Length;
+            bool swapped;
+            for (i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap arr[j] and arr[j+1] 
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                // IF no two elements were  
+                // swapped by inner loop, then break 
+                if (swapped == false)
+                    break;
+            }
+        }
+    }
+}
