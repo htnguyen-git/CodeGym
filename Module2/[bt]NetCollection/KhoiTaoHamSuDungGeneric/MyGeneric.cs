8c1774b421a207b576cde7b6c34a67c1d0daa987
htnguyen-git <htnguyen-git>
2020-05-26 10:19:30 +0700

    Su dung Generic de so sanh 2 doi tuong trong 49p

---
+++ Module2/[bt]NetCollection/KhoiTaoHamSuDungGeneric/MyGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace KhoiTaoHamSuDungGeneric
+{
+    static class MyGeneric<T> 
+    {
+        static public bool SoSanh(T x, T y)
+        {
+            return x.Equals(y) ? true : false;
+        }
+        
+    }
+}
---
+++ Module2/[bt]NetCollection/KhoiTaoHamSuDungGeneric/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KhoiTaoHamSuDungGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "asdfasdf";
+            string y = "asdfasdf";
+            bool a = MyGeneric<string>.SoSanh(x, y);
+            Console.WriteLine(a);
+
+            int t = 6;
+            int k = 10;
+            bool b = MyGeneric<int>.SoSanh(t,k);
+            Console.WriteLine(b);
+        }
+    }
+}
