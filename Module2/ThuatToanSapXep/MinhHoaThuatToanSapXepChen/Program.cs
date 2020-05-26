571d774ae08a1395ee6d9775168731eecc0858c8
htnguyen-git <htnguyen-git>
2020-05-26 16:30:31 +0700

    10p minh hoa thuat toan sap xep chen

---
+++ Module2/ThuatToanSapXep/MinhHoaThuatToanSapXepChen/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MinhHoaThuatToanSapXepChen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 5, 4, 2, 1, 3, 4 };
+            InsertionSort(arr);
+        }
+        static void InsertionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                // Move elements of arr[0..i-1], 
+                // that are greater than key, 
+                // to one position ahead of 
+                // their current position 
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+                string s = "";
+                foreach (var element in arr)
+                {
+                    s += element + " ";
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
