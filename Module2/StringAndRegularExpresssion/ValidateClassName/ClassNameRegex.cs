c3714be0263d90ca94ea214d3b632fa1b71c0722
htnguyen-git <htnguyen-git>
2020-06-03 09:26:08 +0700

    bai tap validate Class Name trong 26 p

---
+++ Module2/StringAndRegularExpresssion/ValidateClassName/ClassNameRegex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ValidateClassName
+{
+    class ClassNameRegex
+    {
+        private string CLASSNAME_REGEX = "^[ACP][0-9]{4}[G-M]$";
+        public bool Validate(string regex)
+        {
+            return Regex.IsMatch(regex, CLASSNAME_REGEX);
+        }
+    }
+}
---
+++ Module2/StringAndRegularExpresssion/ValidateClassName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValidateClassName
+{
+    class Program
+    {
+        static ClassNameRegex classNameRegex;
+        static string[] validClass = { "C0318G" };
+        static string[] invalidClass = { "M0318G", "P0323A" };
+        static void Main(string[] args)
+        {
+            classNameRegex = new ClassNameRegex();
+            foreach (var item in validClass)
+            {
+                Console.WriteLine($"{item} is {classNameRegex.Validate(item)}");
+            }
+            foreach (var item in invalidClass)
+            {
+                Console.WriteLine($"{item} is {classNameRegex.Validate(item)}");
+            }
+        }
+    }
+}
