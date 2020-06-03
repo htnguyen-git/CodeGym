d582ffac42496321e29e27aa8e7c90e867d29647
htnguyen-git <htnguyen-git>
2020-06-03 09:51:45 +0700

    Bai tap validate phone number trong 15p

---
+++ Module2/StringAndRegularExpresssion/ValidatePhoneNumber/PhoneNumberRegex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ValidatePhoneNumber
+{
+    
+    class PhoneNumberRegex
+    {
+        private string PHONENUMBER_REGEX = "^[(][0-9]{2}[)][-][(][0][0-9]{9}[)]";
+        public bool Validate(string regex)
+        {
+            return Regex.IsMatch(regex, PHONENUMBER_REGEX);
+        }
+
+    }
+}
---
+++ Module2/StringAndRegularExpresssion/ValidatePhoneNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ValidatePhoneNumber
+{
+    class Program
+    {
+        public static PhoneNumberRegex validatePhone;
+        public static string[] valid = { "(84)-(0978489648)" };
+        public static string[] invalid = { "(a4)-(0978489648)", "(84)-(097848964)" };
+        static void Main(string[] args)
+        {
+            validatePhone = new PhoneNumberRegex();
+            foreach (var item in valid)
+            {
+                Console.WriteLine($"{item} is {validatePhone.Validate(item)}");
+            }
+            foreach (var item in invalid)
+            {
+                Console.WriteLine($"{item} is {validatePhone.Validate(item)}");
+            }
+        }
+    }
+}
