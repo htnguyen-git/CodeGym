39bb2adf7914106cb229b1e58243ba7737c30c75
htnguyen-git <htnguyen-git>
2020-06-03 08:32:41 +0700

    Validate email

---
+++ Module2/StringAndRegularExpresssion/ValidateEmail/EmailExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ValidateEmail
+{
+    class EmailExample
+    {
+        private string EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+        public bool Validate(string regex) 
+        {
+            bool matcher = Regex.IsMatch(regex, EMAIL_REGEX);
+            return matcher; 
+        }
+    }
+}
---
+++ Module2/StringAndRegularExpresssion/ValidateEmail/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace ValidateEmail
+{
+    public class Program
+    {
+        static EmailExample emailExample;
+        static string[] validEmail = new string[] { "a@gmail.com", "ab@yahoo.com", "abc@hotmail.com" };
+        static string[] invalidEmail = new string[] { "@gmail.com", "ab@gmail.", "@#abc@gmail.com" };
+        static void Main(string[] args)
+        {
+            emailExample = new EmailExample();
+            foreach (var email in validEmail)
+            {
+                bool isValid = emailExample.Validate(email);
+                Console.WriteLine($"Email {email} is valid : {isValid}");
+            }
+            foreach (var email in invalidEmail)
+            {
+                bool isValid = emailExample.Validate(email);
+                Console.WriteLine($"Email {email} is valid : {isValid}");
+            }
+        }
+    }
+}
