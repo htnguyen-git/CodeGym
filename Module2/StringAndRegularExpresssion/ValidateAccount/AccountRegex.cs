7f285bc95aca3dd05db020d6412d3aacb796145d
htnguyen-git <htnguyen-git>
2020-06-03 08:57:01 +0700

    Bài tập validate account trong 26p

---
+++ Module2/StringAndRegularExpresssion/ValidateAccount/AccountRegex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ValidateAccount
+{
+    class AccountRegex
+    {
+        private string ACCOUNT_REGEX = "^[_0-9a-z]{6,}$";
+        public bool Validate(string regex)
+        {
+            return Regex.IsMatch(regex, ACCOUNT_REGEX);
+        }
+    }
+}
---
+++ Module2/StringAndRegularExpresssion/ValidateAccount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ValidateAccount
+{
+    class Program
+    {
+        private static AccountRegex accountExample;
+        static string[] validAccount = new String[] { "123abc_", "_abc123", "______", "123456", "abcdefgh" };
+        static string[] invalidAccount = new String[] { ".@", "12345", "1234_", "abcde" };
+        static void Main(string[] args)
+        {
+            accountExample = new AccountRegex();
+            foreach (string account in validAccount)
+            {
+                bool isvalid = accountExample.Validate(account);
+                Console.WriteLine("Account is " + account + " is valid: " + isvalid);
+            }
+            foreach (string account in invalidAccount)
+            {
+                bool isvalid = accountExample.Validate(account);
+                Console.WriteLine("Account is " + account + " is valid: " + isvalid);
+            }
+        }
+    }
+}
